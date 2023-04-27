124ec1ad846116497aa8bcfdf60a10e8d9e69620
dahye777 <mbdhpp0119@gmail.com>
2023-04-27 10:06:38 +0900

    + anime.js

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-let ul = document.querySelector("ul");
-let prev = document.querySelector(".prev");
-let next = document.querySelector(".next");
-let lis = ul.querySelectorAll("li");
+const ul = document.querySelector("ul");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const lis = ul.querySelectorAll("li");
+let len = lis.length; //추가가되도 자동 li의 갯수를 세어줌
 
-ul.style.marginLeft = "-100%";
+ul.style.left = "-100%"; //ul의 초기 위치값을 정해주는 코드
+ul.prepend(ul.lastElementChild); // 로딩 후 첫번쨰 li가 frame에 보이도록 초기화하는 코드
 
+//ul의 너비값을 li의 갯수에 맞춰서 자동계산코드
+ul.style.width = `${100 * len}%`;
+//각 li의 너비값을 자동계산해주는 코드
+lis.forEach((el) => {
+  el.style.width = `${100 / len}%`;
+});
+
+//next 슬라이드 이동 코드
 next.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // 이 다음 슬라이더로 이동을 시키는 코드작성
-  ul.style.marginLeft = "-200%";
-  ul.append(ul.firstElementChild);
+  new Anim(ul, {
+    prop: "left",
+    value: "-200%",
+    duration: 1000,
+    callback: () => {
+      ul.append(ul.firstElementChild); // 첫번째 li를  뒤쪽으로 보내는 코드
+      ul.style.left = "-100%"; //-200%에서 초기 위치 값으로 복귀하도록 하는 코드
+    },
+  });
 });
 
+//prev 슬라이드 이동 코드
 prev.addEventListener("click", (e) => {
   e.preventDefault();
 
-  // 이 다음 슬라이더로 이동을 시키는 코드작성
-  ul.style.marginLeft = "0%";
-  ul.prepend(ul.lastElementChild);
+  new Anim(ul, {
+    prop: "left",
+    value: "0%",
+    duration: 1000,
+    callback: () => {
+      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀하는 코드
+      ul.prepend(ul.lastElementChild); //마지막 li를 맨앞으로 보내는 코드
+    },
+  });
 });
 
 /*
@@ -46,4 +69,16 @@ data속성이란??
 개발자가 속성값으로 특정 정보를 은닉하는 방법입니다
 DOM요소에는 영향을 주지 않습니다 data-이름을 자유롭게 작성 = "";
 data 속성으로 지정하고, 값을 넣은 값은 개발자가 자유롭게 활용이 가능합니다
+
+loop슬라이더의 기본 로직
+1. 초기 ul의 위치값을 left, margin-left가 -100%로 설정한다
+=> loop가 되려면 반드시 보여지는 패널의 앞뒤에 최소 1개의 패널이 존재해야한다
+2. 슬라이드의 기본 모션
+prev버튼 클릭시 -100% -> 0%
+next버튼 클릭시 -100% -> -200%
+3. 이동이 끝나고 나서는
+앞이나 뒤에 쌓인 패널을 다시 앞이나 뒤로 재배치를 해줘야한다 => loop가 되려면
+하지만 -200%, 0% 상태이기 때문에 다시 -100%인 상태로 변경을 해주어야
+우리가 보려는 패널이 정상적으로 보이게 됨
+4. ul의 초기위치를 항상 다시 -100%로 초기화 한다
 */
