f0f8112c154d313ded76ae0c3958f04028eac265
dahye777 <mbdhpp0119@gmail.com>
2023-04-27 11:05:00 +0900

    최종 수정

--- main.js
+++ main.js
@@ -3,45 +3,28 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 const lis = ul.querySelectorAll("li");
 let len = lis.length; //추가가되도 자동 li의 갯수를 세어줌
+let enableClick = true;
 
-ul.style.left = "-100%"; //ul의 초기 위치값을 정해주는 코드
-ul.prepend(ul.lastElementChild); // 로딩 후 첫번쨰 li가 frame에 보이도록 초기화하는 코드
-
-//ul의 너비값을 li의 갯수에 맞춰서 자동계산코드
-ul.style.width = `${100 * len}%`;
-//각 li의 너비값을 자동계산해주는 코드
-lis.forEach((el) => {
-  el.style.width = `${100 / len}%`;
-});
+init();
 
 //next 슬라이드 이동 코드
 next.addEventListener("click", (e) => {
   e.preventDefault();
 
-  new Anim(ul, {
-    prop: "left",
-    value: "-200%",
-    duration: 1000,
-    callback: () => {
-      ul.append(ul.firstElementChild); // 첫번째 li를  뒤쪽으로 보내는 코드
-      ul.style.left = "-100%"; //-200%에서 초기 위치 값으로 복귀하도록 하는 코드
-    },
-  });
+  if (enableClick) {
+    nextSlide();
+    enableClick = false;
+  }
 });
 
 //prev 슬라이드 이동 코드
 prev.addEventListener("click", (e) => {
   e.preventDefault();
 
-  new Anim(ul, {
-    prop: "left",
-    value: "0%",
-    duration: 1000,
-    callback: () => {
-      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀하는 코드
-      ul.prepend(ul.lastElementChild); //마지막 li를 맨앞으로 보내는 코드
-    },
-  });
+  if (enableClick) {
+    prevSlide();
+    enableClick = false;
+  }
 });
 
 /*
@@ -82,3 +65,39 @@ next버튼 클릭시 -100% -> -200%
 우리가 보려는 패널이 정상적으로 보이게 됨
 4. ul의 초기위치를 항상 다시 -100%로 초기화 한다
 */
+function prevSlide() {
+  new Anim(ul, {
+    prop: "left",
+    value: "0%",
+    duration: 1000,
+    callback: () => {
+      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀하는 코드
+      ul.prepend(ul.lastElementChild); //마지막 li를 맨앞으로 보내는 코드
+    },
+  });
+}
+
+function nextSlide() {
+  new Anim(ul, {
+    prop: "left",
+    value: "-200%",
+    duration: 1000,
+    callback: () => {
+      ul.style.left = "-100%"; //0%에서 초기 위치 값으로 복귀하는 코드
+      ul.append(ul.firstElementChild); //마지막 li를 맨앞으로 보내는 코드
+      enableClick = true;
+    },
+  });
+}
+
+function init() {
+  ul.style.left = "-100%"; //ul의 초기 위치값을 정해주는 코드
+  ul.prepend(ul.lastElementChild); // 로딩 후 첫번쨰 li가 frame에 보이도록 초기화하는 코드
+
+  //ul의 너비값을 li의 갯수에 맞춰서 자동계산코드
+  ul.style.width = `${100 * len}%`;
+  //각 li의 너비값을 자동계산해주는 코드
+  lis.forEach((el) => {
+    el.style.width = `${100 / len}%`;
+  });
+}
